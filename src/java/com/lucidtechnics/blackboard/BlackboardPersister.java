b9d2d462c62a28e12583d11cfd1e77bac1ca99a6
bediako.george@lucidtechnics.com <bediako.george@lucidtechnics.com@a217a68c-2ea5-11de-ba90-e1c2dc4d389a>
2009-06-17 17:38:39 +0000

    setting up to use Json Persistence

---
+++ src/java/com/lucidtechnics/blackboard/BlackboardPersister.java
@@ -0,0 +1,46 @@
+package com.lucidtechnics.blackboard;
+
+import com.db4o.Db4o;
+import com.db4o.ObjectServer;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class BlackboardPersister
+   implements Persister
+{
+	private static Log logger = LogFactory.getLog(BlackboardPersister.class);
+	
+	private Blackboard blackboard;
+	private String persistenceDir;
+
+	public Blackboard getBlackboard() {  return blackboard; }
+	private String getPersistenceDir() { return persistenceDir; }
+	
+	public void setBlackboard(Blackboard _blackboard) { blackboard = _blackboard; }
+	private void setPersistenceDir(String _persistenceDir) { persistenceDir = persistenceDir; }
+	
+	public BlackboardPersister(Blackboard _blackboard, String _persistenceDir)
+	{
+		setBlackboard(_blackboard);
+		setPersistenceDir(_persistenceDir);
+	}
+
+	public void init()
+	{		
+	}
+
+	public TargetSpace get(Object _workspaceIdentifier)
+	{
+		return new TargetSpaceImpl();
+	}
+
+	public void put(TargetSpace _targetSpace)
+	{
+		String json = new com.google.gson.Gson().toJson(_targetSpace);
+
+		System.out.println(json);
+	}
+}
--- src/java/com/lucidtechnics/blackboard/PersisterFactory.java
+++ src/java/com/lucidtechnics/blackboard/PersisterFactory.java
@@ -4,8 +4,18 @@ public class PersisterFactory
 {
 	public static Persister make(String _type, String _persistenceDir, Blackboard _blackboard)
 	{
-		Persister persister = new Db4oPersister(_blackboard, _persistenceDir);
-		persister.init();
+		Persister persister = null;
+				
+		if ("db4o".equalsIgnoreCase(_type) == true)
+		{
+			persister = new Db4oPersister(_blackboard, _persistenceDir);
+			persister.init();
+		}
+		else
+		{
+			persister = new BlackboardPersister(_blackboard, _persistenceDir);
+			persister.init();
+		}			
 		
 		return persister; 
 	}
