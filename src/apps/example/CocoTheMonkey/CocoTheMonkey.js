eb2c493761457f74a2e1e6262b88092bdfba8a69
bediako.george@lucidtechnics.com <bediako.george@lucidtechnics.com@a217a68c-2ea5-11de-ba90-e1c2dc4d389a>
2009-10-20 17:07:44 +0000

    made blackboard more robust. Example driver sends more events.

--- src/apps/example/CocoTheMonkey/CocoTheMonkey.js
+++ src/apps/example/CocoTheMonkey/CocoTheMonkey.js
@@ -2,12 +2,12 @@ LOGGER.info("Starting Coco The Monkey");
 
 thread1 = new Packages.java.lang.Thread(function run()
 {
-	for (var i = 0; i < 200; i++)
+	for (var i = 0; i < 10000; i++)
 	{						
-		monkey = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Monkey("Coco-1-" + i);
-		mango = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Mango("Coco-2-" + i);
-		eagle = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Eagle("Coco-3-" + i);
-		hunter = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Hunter("Coco-4-" + i);
+		var monkey = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Monkey("Coco-1-" + i);
+		var mango = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Mango("Coco-2-" + i);
+		var eagle = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Eagle("Coco-3-" + i);
+		var hunter = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Hunter("Coco-4-" + i);
 
 		WORKSPACE.placeOnBlackboard(eagle);
 		WORKSPACE.placeOnBlackboard(mango);
@@ -19,12 +19,12 @@ thread1 = new Packages.java.lang.Thread(function run()
 
 thread2 = new Packages.java.lang.Thread(function run()
 {
-	for (var i = 0; i < 200; i++)
+	for (var i = 0; i < 10000; i++)
 	{						
-		monkey = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Monkey("Coco-4-" + i);
-		mango = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Mango("Coco-1-" + i);
-		eagle = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Eagle("Coco-2-" + i);
-		hunter = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Hunter("Coco-3-" + i);
+		var monkey = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Monkey("Coco-4-" + i);
+		var mango = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Mango("Coco-1-" + i);
+		var eagle = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Eagle("Coco-2-" + i);
+		var hunter = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Hunter("Coco-3-" + i);
 
 		WORKSPACE.placeOnBlackboard(eagle);
 		WORKSPACE.placeOnBlackboard(mango);
@@ -36,12 +36,12 @@ thread2 = new Packages.java.lang.Thread(function run()
 
 thread3 = new Packages.java.lang.Thread(function run()
 {
-	for (var i = 0; i < 200; i++)
+	for (var i = 0; i < 10000; i++)
 	{						
-		monkey = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Monkey("Coco-3-" + i);
-		mango = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Mango("Coco-4-" + i);
-		eagle = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Eagle("Coco-1-" + i);
-		hunter = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Hunter("Coco-2-" + i);
+		var monkey = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Monkey("Coco-3-" + i);
+		var mango = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Mango("Coco-4-" + i);
+		var eagle = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Eagle("Coco-1-" + i);
+		var hunter = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Hunter("Coco-2-" + i);
 
 		WORKSPACE.placeOnBlackboard(eagle);
 		WORKSPACE.placeOnBlackboard(mango);
@@ -53,12 +53,12 @@ thread3 = new Packages.java.lang.Thread(function run()
 
 thread4 = new Packages.java.lang.Thread(function run()
 {
-	for (var i = 0; i < 200; i++)
+	for (var i = 0; i < 10000; i++)
 	{						
-		monkey = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Monkey("Coco-2-" + i);
-		mango = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Mango("Coco-3-" + i);
-		eagle = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Eagle("Coco-4-" + i);
-		hunter = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Hunter("Coco-1-" + i);
+		var monkey = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Monkey("Coco-2-" + i);
+		var mango = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Mango("Coco-3-" + i);
+		var eagle = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Eagle("Coco-4-" + i);
+		var hunter = new Packages.com.lucidtechnics.blackboard.examples.cocothemonkey.Hunter("Coco-1-" + i);
 
 		WORKSPACE.placeOnBlackboard(eagle);
 		WORKSPACE.placeOnBlackboard(mango);
